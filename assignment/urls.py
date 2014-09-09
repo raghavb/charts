867b3dd90eff3a1669029e698d257e158e7f19fc
raghavb <raghavbehl@gmail.com>
2014-09-09 18:44:09 -0500

    urls changes to add static 2

--- assignment/urls.py
+++ assignment/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.conf import settings
 
 #Django URL displatcher. Helps to route the application.
 urlpatterns = patterns('',
@@ -12,5 +13,6 @@ url(r'^search$', 'assignment.views.search', name='search'),
 url(r'^upload$', 'assignment.views.upload', name='upload'),
 url(r'^.*', 'assignment.views.raise404', name='404'),
 )
+
 if settings.DEBUG:
    urlpatterns += staticfiles_urlpatterns() 
