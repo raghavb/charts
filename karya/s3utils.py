92b728a45316da65733ff20fb69edcbd40d8db1e
raghavb <raghavbehl@gmail.com>
2014-09-09 23:26:29 -0500

    s3

--- karya/s3utils.py
+++ karya/s3utils.py
@@ -1,3 +1,7 @@
+from django.conf import settings
 from storages.backends.s3boto import S3BotoStorage
 
-MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='/')
+MediaRootS3BotoStorage  = lambda: S3BotoStorage(
+  location=settings.MEDIA_ROOT,
+    file_overwrite=False
+    )
